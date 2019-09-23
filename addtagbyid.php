acb034a0e5c4cd8071efa10969bd2680e6dab9fe
johnomahoy <53034160+johnomahoy@users.noreply.github.com>
2019-09-23 17:11:18 +0800

    Update addtagbyid.php

--- addtagbyid.php
+++ addtagbyid.php
@@ -7,19 +7,22 @@ $app = new iSDK;
 
 if( $app->cfgCon("aa478")){ 
 
+	//Get the data
 $contactId = $_REQUEST['contactId'];
-echo $contactId;
 $stringB=$_REQUEST['tagIDs'];
 
+	//Get all the data from the array of tags
 $arrayB = explode(',', $stringB);
 
-
+	//Loop and insert the data using the contacId
 foreach($arrayB as $arraybb){
 	echo $arraybb."<br>";
 	$result = $app->grpAssign($contactId, $arraybb);
 	if(!$result){
+		//Show if error
 		echo "Error";
 	}else{
+		//Show if success
 		echo "Success";
 	}
 } 
